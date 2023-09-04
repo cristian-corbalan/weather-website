c1e4e58ee0a0cf51b0e7f1a6379715d4b8178ddd
cristian-corbalan <ccorbalandw@gmail.com>
2023-09-04 19:19:45 -0300

    Create function to get the searched location coordinates

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 "use strict"
 
-const search = (e) => {
+// -------------------- Event functions:
+
+const search = async (e) => {
     e.preventDefault()
 
     let value = document.getElementById('searchInput').value;
@@ -8,6 +10,23 @@ const search = (e) => {
     console.info('The value to search is:', value);
 
     // console.info('Searching...');
+
+    let coordinates = await getCoordinates(value);
+    console.log(coordinates);
+}
+
+// ---------- Functions:
+
+/**
+ * Use the API geocoder to convert city names or zip-codes to geo coordinates.
+ * @param {string} location The city name or a zip-code.
+ * @return {Promise}
+ */
+const getCoordinates = (location) => {
+    const COORDINATES_API = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${API_KEY}`;
+
+    return fetch(COORDINATES_API)
+        .then(res => res.json());
 }
 
 // ---------- API variables:
@@ -15,6 +34,7 @@ const search = (e) => {
 const API_KEY = `1bff7579067727b7399aa930fbeb7b4c`;
 
 // ---------- Add event to the form:
+
 const searchForm = document.getElementById('searchForm');
 
 searchForm.addEventListener('submit', search);
